69f7a8dd88f9a0833f85fcaa7db2dd5f71453074
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-09-27 17:20:20 +0200

    WIP

--- lua/nvim-md-todo-toggle.lua
+++ lua/nvim-md-todo-toggle.lua
@@ -2,6 +2,10 @@ local module = require("nvim-md-todo-toggle")
 
 local M = {}
 M.config = {}
-M.setup = module.setup
+M.setup = function(args)
+  M.config = vim.tbl_deep_extend("force", M.config, args or {})
+end
+
+M.toggle = module.toggle
 
 return M
--- lua/nvim-md-todo-toggle/init.lua
+++ lua/nvim-md-todo-toggle/init.lua
@@ -1,10 +1,6 @@
-local function setup(parameters)
-  print("printing from setup()")
-end
-
 print("I have been started")
 
-local function toggle_todo_marker()
+local function toggle()
   local line = vim.api.nvim_get_current_line()
   local opened, _ = string.find(line, "- %[ %]")
   local closed, _ = string.find(line, "- %[x%]")
@@ -16,17 +12,17 @@ local function toggle_todo_marker()
     vim.cmd(':.,.s/\\[x\\]/[ ]/')
   end
   vim.cmd(":noh")
-  vim.cmd(":call cursor(" .. cursor_pos[1] .. ", " .. cursor_pos[2]+1 .. ")")
+  vim.cmd(":call cursor(" .. cursor_pos[1] .. ", " .. cursor_pos[2] + 1 .. ")")
 end
 
-vim.keymap.set("n", "<leader>t", toggle_todo_marker, { desc = "jurst ran it", noremap = true, silent = true })
-
-vim.api.nvim_create_user_command(
-  'TDToggle',
-  toggle_todo_marker,
-  { bang = true, desc = 'bang, what is bang?' }
-)
+--vim.keymap.set("n", "<leader>t", toggle, { desc = "jurst ran it", noremap = true, silent = true })
+--
+--vim.api.nvim_create_user_command(
+--  'TDToggle',
+--  toggle,
+--  { bang = true, desc = 'bang, what is bang?' }
+--)
 
 return {
-  setup = setup,
+  toggle
 }
