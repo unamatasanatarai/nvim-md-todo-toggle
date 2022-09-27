e437146097c504033bdf25eaa90e6e93d835557c
Unamata Sanatarai <unamatasanatarai@gmail.com>
2022-09-27 17:13:07 +0200

    WIP

---
+++ lua/nvim-md-todo-toggle.lua
@@ -0,0 +1,7 @@
+local module = require("nvim-md-todo-toggle")
+
+local M = {}
+M.config = {}
+M.setup = module.setup
+
+return M
---
+++ plugin/nvim-md-todo-toggle.lua
@@ -0,0 +1,2 @@
+vim.api.nvim_create_user_command("TDToggle", require("nvim-md-todo-toggle").setup(), {})
+
